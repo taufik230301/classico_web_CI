a5a7f971acf158172327fb181b02039582e3fae9
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2021-11-04 19:40:26 +0700

    Fix Form Tambah

--- application/views/home.php
+++ application/views/home.php
@@ -56,7 +56,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Form Tambah</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
